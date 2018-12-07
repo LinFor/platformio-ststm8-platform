a4fcb899c7a72be722b99a6dabe2519375ac1624
Kirill Shashlov <Kirill.Shashlov@kaspersky.com>
2018-12-07 18:08:11 +0300

    fix

--- builder/frameworks/spl8s.py
+++ builder/frameworks/spl8s.py
@@ -9,16 +9,6 @@ platform = env.PioPlatform()
 FRAMEWORK_DIR = platform.get_package_dir("framework-spl8s")
 assert isdir(FRAMEWORK_DIR)
 
-cpppaths = ['.']
-
-cpppaths.append(
-    join(FRAMEWORK_DIR, "STM8S_StdPeriph_Lib", "Libraries", "STM8S_StdPeriph_Driver", "inc")
-)
-cpppaths.append(
-    join(FRAMEWORK_DIR, "STM8S_StdPeriph_Lib", "Libraries", "STM8S_StdPeriph_Driver", "src")
-)
-cpppaths.append(
-    join(FRAMEWORK_DIR, "STM8S_StdPeriph_Lib", "Project", "STM8S_StdPeriph_Template")
-)
-
-env.Append(CPPPATH=cpppaths)
+env.Append(CPPPATH=join(FRAMEWORK_DIR, "STM8S_StdPeriph_Lib", "Libraries", "STM8S_StdPeriph_Driver", "inc"))
+env.Append(CPPPATH=join(FRAMEWORK_DIR, "STM8S_StdPeriph_Lib", "Libraries", "STM8S_StdPeriph_Driver", "src"))
+env.Append(CPPPATH=join(FRAMEWORK_DIR, "STM8S_StdPeriph_Lib", "Project", "STM8S_StdPeriph_Template"))
